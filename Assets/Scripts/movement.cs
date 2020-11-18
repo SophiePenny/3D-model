18bb6a7e7a8d7bf158e7d70cf73f7ab3f61690e6
Sophie Penny <512894@escg.ac.uk>
2020-11-18 16:10:08 +0000

    Initial commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Velocity = rb.velocity;
+        if (Input.GetKey("a"))
+        {
+            Velocity.x = -3;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            Velocity.x = 3;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            Velocity.z = 3;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            Velocity.z = -3;
+        }
+
+        if (Input.GetKey("space"))
+        {
+            Velocity.y = 3;
+        }
+
+        rb.velocity = Velocity;
+    }
+}
