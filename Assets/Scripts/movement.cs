61a12d6ef6925dde97ca4821888189984117c238
Sophie Penny <512894@escg.ac.uk>
2020-12-02 16:11:49 +0000

    it sort of works?

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     Rigidbody rb;
+    public GameObject Sphere;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    public void throwball()
+    {
+        //Debug.Log("throw");
+
+        Instantiate(Sphere, new Vector3(-0.158f , -0.407f , -0.351f), Quaternion.identity);
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 Velocity = rb.velocity;    
+        Vector3 Velocity = rb.velocity;
+
+
+        //throw ball
+        anim.SetBool("idle", true);
+        anim.SetBool("throw", false);
+
+        if(Input.GetKeyDown("space"))
+        {
+            anim.SetBool("throw", true);
+            anim.SetBool("idle", false);
+        }
+
+        // move player
         if (Input.GetKey("d"))
         {
             Velocity.x = -3;
@@ -35,7 +61,7 @@ public class movement : MonoBehaviour
             Velocity.z = -3;
         }
 
-        if (Input.GetKey("space"))
+        if (Input.GetKey("q"))
         {
             if (Velocity.y == 0)
             {
